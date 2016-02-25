6a314ed764fe10105f6d4de4b31c8a15eb9514c9
ElliotShi <elliotshi@outlook.com>
2016-02-25 11:15:37 +0800

    answered for home page

--- js/script.js
+++ js/script.js
@@ -8,39 +8,39 @@ $("#prepend button").click(function() {
 });
 
 $("#before button").click(function() {
-	
+  $("#before p").before("<strong>Hello!</strong>");
 });
 
 $("#css1 button").click(function() {
-	
+  $("#css1 p").css("font-size", "20px");
 });
 
 $("#css2 button").click(function() {
-	
+  $("#css2 p").css({"font-size":"20px","color":"blue"});
 });
 
 $("#attr1 button").click(function() {
-	
+  console.log($("#attr1 a").attr("href"));
 });
 
 $("#attr2 button").click(function() {
-
+  $("#attr2 a").attr("href", "http://www.google.com")
 });
 
 $("#class1 button").click(function() {
-	
+  $("#class1 p").addClass("bg-primary");
 });
 
 $("#class2 button").click(function() {
-
+  alert($("#class2 p").hasClass("lead"));
 });
 
 $("#form1 button").click(function() {
-
+  console.log($("#form1 input").val());
 });
 
 $("#form2 button").click(function() {
-
+  $("#form2 input").val("Elliot");
 });
 
 // handle the mouseover event here
