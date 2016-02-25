68b613550106ba1d969e86360b28f17a17954d53
ElliotShi <elliotshi@outlook.com>
2016-02-25 14:56:29 +0800

    answers for events and animate

--- js/script.js
+++ js/script.js
@@ -44,9 +44,61 @@ $("#form2 button").click(function() {
 });
 
 // handle the mouseover event here
+$("#mouse img").mouseover(function(){
+  $("#mouse img").attr("style", "width:50%");
+})
 
 // handle the form events here
+$(".form-group button").click(function(event){
+  event.preventDefault();
+  console.log("email: " + $("form input[type=email]").val());
+  console.log("password: " + $("form input[type=password]").val());
+  debugger;
+  console.log("checkbox: " + $("form input[type=checkbox]").prop("checked"));
+})
 
+//animate increase size
+$("#animate1 img").mouseover(function(){
+  $("#animate1 img").animate({
+    width: "70%"
+  })
+})
+
+$("#animate1 img").mouseout(function(){
+  $("#animate1 img").animate({
+    width: "20%"
+  })
+})
+
+
+//animate double click
+var flag = true;
+$("#animate2 img").dblclick(function(){
+  if(flag){
+    $("#animate2 img").animate({
+      "margin-left": "900px",
+    })
+    flag = false;
+  }else{
+    $("#animate2 img").animate({
+      "margin-left": "0",
+    })
+    flag = true;
+  }
+})
+
+
+//fade out fade in
+$("#animate3 img").click(function(){
+  $("#animate3 img").animate({
+    opacity: 0
+  }, function() {
+    $(this).attr("src", "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg")
+    $(this).animate({opacity:1}, 2000);
+  })
+})
+
+//submit
 $("#formEvents form").submit(function(event) {
 
 });
